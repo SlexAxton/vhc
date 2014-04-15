3f7bd72f17da8a4f66b61c89c3c75670bda65701
Alex Sexton <alexsexton@gmail.com>
2014-04-15 00:55:05 -0500

    Mostly working dev env

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,54 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'build/static/js/<%= pkg.name %>.js',
+        dest: 'build/static/js/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'static/js/**/*.js'
+      ]
+    },
+    clean: ['build'],
+    watch: {
+      scripts: {
+        files: ['static/js/**/*.js'],
+        tasks: ['jshint', 'browserify:dev'],
+        options: {
+          spawn: false
+        }
+      },
+      markup: {
+        files: ['src/**/*.html', 'src/**/*.md', 'src/**/*.html'],
+        tasks: ['shell:metalsmith'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    shell: {
+      metalsmith: {
+        command: 'node_modules/.bin/metalsmith'
+      }
+    },
+    browserify: {
+      dev: {
+        src: ['static/js/<%= pkg.name %>.js'],
+        dest: 'build/static/app.js',
+        options: {
+          watch: true
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-shell-spawn');
 };
